11c28333479106ef8f24f00990e279cd1e6f97e9
Wallace Rodrigues <wallacesilvarodrigues01@gmail.com>
2025-10-14 20:17:23 -0300

    retirando mapa

---
+++ src/components/endereco/index.tsx
@@ -0,0 +1,13 @@
+export function Endereco() {
+  return (
+    <div className="flex flex-col justify-center items-center gap-7 pb-10 p-3 bg-[#DBDBDB]">
+      <div className="flex flex-col">
+        <h3 className="text-center text-2xl font-medium">Endereço</h3>
+        <span className="text-center mt-5 text-xl">
+          Rua 10, Quadra 02, Lote 05-B, Sala 01, Galeria 10, Conjunto Uirapuru,
+          Senador Canedo - GO, CEP: 75261-211
+        </span>
+      </div>
+    </div>
+  );
+}
